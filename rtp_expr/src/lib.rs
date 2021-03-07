27f0fe442e5cc5d2314a022d9f8f56abce6fe4ff
schulke-214 <hello@maximilianschulke.com>
2021-03-07 14:59:21 +0100

    export utilities to quickly run queries

--- rtp_expr/src/lib.rs
+++ rtp_expr/src/lib.rs
@@ -1,9 +1,21 @@
+mod error;
 mod lexer;
 mod logical_operator;
 mod parser;
 mod query;
 mod runtime;
 
+use error::RTPExpressionResult;
+
+
+pub use runtime::run;
+
+pub fn into_ast(source: String) -> RTPExpressionResult<parser::AST> {
+	let tokens = lexer::lex(source)?;
+	let ast = parser::parse(tokens)?;
+
+	Ok(ast)
+}
 
 #[cfg(test)]
 mod tests {
